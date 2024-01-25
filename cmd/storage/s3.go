fdd9d8faa4aa329693dd08a20f507d5db86e7f42
Nikolai Gotzmann <self@ngotzmann.net>
2024-01-25 14:07:41 +0100

    Return error on client init

--- cmd/storage/s3.go
+++ cmd/storage/s3.go
@@ -17,7 +17,7 @@ type S3Storage struct {
 	Bucket    string `yaml:"bucket"`
 }
 
-func (s3 S3Storage) initClient(s3Config config.S3) *minio.Client {
+func (s3 S3Storage) initClient(s3Config config.S3) (*minio.Client, error) {
 	log.Info("Initializing the S3 Client.")
 	useSSL := true
 
@@ -27,28 +27,31 @@ func (s3 S3Storage) initClient(s3Config config.S3) *minio.Client {
 		Creds:  creds,
 		Secure: useSSL,
 	}
-	// Initialize minio client object.
 	minioClient, err := minio.New(s3Config.Endpoint, options)
 	if err != nil {
-		log.Errorf("Connection to S3 Storage at %s failed.", s3Config.Endpoint)
-		log.Error(err)
+		log.Errorf("Connection to S3 Storage at %s failed.", cfg.Endpoint)
+		return nil, err
 	} else {
 		log.Infof("Connection to S3 Storage at %s succeeded.", s3Config.Endpoint)
 	}
-	return minioClient
+	return minioClient, nil
 }
 
 func (s3 S3Storage) Upload(storageCfg config.Storage, file string) error {
 	cfg := storageCfg.S3Config
 	ctx := context.Background()
 
-	minioClient := s3.initClient(cfg)
+	minioClient, err := s3.initClient(cfg)
+
+	if err != nil {
+		return err
+	}
 
 	contentType := "application/octet-stream"
 	options := minio.PutObjectOptions{ContentType: contentType}
 
 	log.Infof("Uploading file %s to %s.", file, minioClient.EndpointURL())
-	_, err := minioClient.FPutObject(ctx, cfg.Bucket, file, file, options)
+	_, err = minioClient.FPutObject(ctx, cfg.Bucket, file, file, options)
 	if err != nil {
 		log.Errorf("Uploading file %s to %s failed.", file, minioClient.EndpointURL())
 		log.Error(err)
@@ -63,8 +66,11 @@ func (s3 S3Storage) Delete(storageCfg config.Storage, file string) error {
 	cfg := storageCfg.S3Config
 	ctx := context.Background()
 
-	minioClient := s3.initClient(cfg)
-	err := minioClient.RemoveObject(ctx, cfg.Bucket, file, minio.RemoveObjectOptions{})
+	minioClient, err := s3.initClient(cfg)
+	if err != nil {
+		return err
+	}
+	err = minioClient.RemoveObject(ctx, cfg.Bucket, file, minio.RemoveObjectOptions{})
 	if err != nil {
 		log.Errorf("Removing object %s failed.", file)
 		log.Error(err)
@@ -78,7 +84,10 @@ func (s3 S3Storage) RetentionDelete(dbConfig config.Database) error {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	minioClient := s3.initClient(cfg)
+	minioClient, err := s3.initClient(cfg)
+	if err != nil {
+		return err
+	}
 
 	log.Infof("Getting objects of Storage at %s in bucket %s.", minioClient.EndpointURL(), cfg.Bucket)
 	objects := minioClient.ListObjects(ctx, cfg.Bucket, minio.ListObjectsOptions{
